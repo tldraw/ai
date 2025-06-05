dccaea21cc26e22dcf2379dd4fcbb6646cc44356
Steve Ruiz <steveruizok@gmail.com>
2025-06-05 08:42:05 -0700

    delete everything and move to tldraw monorepo

--- package/src/useTldrawAiModule.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react'
-import { TldrawAiModule, TldrawAiModuleOptions } from './TldrawAiModule'
-
-export function useTldrawAiModule(options: TldrawAiModuleOptions) {
-	const ai = useMemo(() => new TldrawAiModule(options), [options])
-	return ai
-}
--- package/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-	test: {
-		environment: 'happy-dom',
-		include: ['**/*.test.ts', '**/*.test.tsx'],
-		globals: true,
-	},
-})
